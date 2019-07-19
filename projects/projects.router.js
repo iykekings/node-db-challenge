923d5b6aa2e7b9fc9b4b82087a2b43f3811db13e
Ikechukwu <iykekings36@gmail.com>
2019-07-19 10:11:59 +0100

    add validateProjectId middleware

--- projects/projects.router.js
+++ projects/projects.router.js
@@ -22,4 +22,19 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// middlewares
+const validateProjectId = async (req, res, next) => {
+  try {
+    const project = await getProjects(req.params.id);
+    if (project) {
+      next();
+    }
+    res
+      .status(400)
+      .json({ message: "The project_id for the action doesn't exist" });
+  } catch (error) {
+    res.status(500).json({ message: 'Could not validate the Id' });
+  }
+};
+
 module.exports = router;
