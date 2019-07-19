ec2387156923a443a6d291324b03d444021f1d06
Ikechukwu <iykekings36@gmail.com>
2019-07-19 08:41:38 +0100

    created and run migration

--- data/migrations/20190719081800_projects.js
+++ data/migrations/20190719081800_projects.js
@@ -1,8 +1,30 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
+      tbl
+        .text('name', 128)
+        .unique()
+        .notNullable();
+      tbl.text('description').notNullable();
+      tbl.boolean('completed').defaultTo(false);
+    })
+    .createTable('actions', tbl => {
+      tbl.increments();
+      tbl.text('notes').notNullable();
+      tbl.text('description').notNullable();
+      tbl.boolean('completed').defaultTo(false);
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('projects').dropTableIfExists('actions');
 };
